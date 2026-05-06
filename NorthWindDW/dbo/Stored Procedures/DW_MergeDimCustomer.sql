b602023c6b5ab3d29169df37ea883a6f2ac5a59e
Edwin Roque Cerrogrande <201706711@est.umss.edu>
2026-05-05 23:03:02 -0400

    Add merge procedures for DW, ETL project creation, and the package

---
+++ NorthWindDW/dbo/Stored Procedures/DW_MergeDimCustomer.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimCustomer]
+AS
+BEGIN
+    -- Actualiza los datos descriptivos en el DW desde el Staging
+    UPDATE dc
+    SET dc.[CompanyName] = sc.[CompanyName],
+        dc.[ContactName] = sc.[ContactName],
+        dc.[ContactTitle] = sc.[ContactTitle],
+        dc.[Address] = sc.[Address],
+        dc.[City] = sc.[City],
+        dc.[Region] = sc.[Region],
+        dc.[PostalCode] = sc.[PostalCode],
+        dc.[Country] = sc.[Country],
+        dc.[Phone] = sc.[Phone],
+        dc.[Fax] = sc.[Fax],
+        dc.[CustomerDesc] = sc.[CustomerDesc]
+    FROM [dbo].[DimCustomer] dc
+    INNER JOIN [staging].[Customer] sc ON (dc.[CustomerSK] = sc.[CustomerSK]);
+END;
---
+++ NorthWindDW/scripts/Load-data-package.PostDeployment.sql
@@ -0,0 +1,30 @@
+﻿EXEC sp_MSforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL';
+GO
+
+INSERT INTO dbo.PackageConfig
+(PackageID, TableName, LastRowVersion)
+VALUES(1, N'Customers', 56011);
+GO
+
+INSERT INTO dbo.PackageConfig
+(PackageID, TableName, LastRowVersion)
+VALUES(2, N'Products', 56014);
+GO
+
+INSERT INTO dbo.PackageConfig
+(PackageID, TableName, LastRowVersion)
+VALUES(3, N'Employees', 56011);
+GO
+
+INSERT INTO dbo.PackageConfig
+(PackageID, TableName, LastRowVersion)
+VALUES(4, N'Orders', 78000);
+GO
+
+INSERT INTO dbo.PackageConfig
+(PackageID, TableName, LastRowVersion)
+VALUES(5, N'Shippers', 56021);
+GO
+
+EXEC sp_MSforeachtable 'ALTER TABLE ? CHECK CONSTRAINT ALL';
+GO
